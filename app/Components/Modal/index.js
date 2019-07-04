73d4e5dd31d6b52c4e134684b5d16813a0f33945
Tijo <tijo@QB-TP3-1605.local>
2019-07-04 13:31:17 +0530

    Added Modal and styles for photo grid/modal

---
+++ app/Components/Modal/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+    View,
+    Modal
+} from 'react-native';
+import Styles from '../../Styles'
+const ModalComponent = ({
+    transparent,
+    animationType, visible, ShowModalFunction, imageuri, ModalVisibleStatus
+}) => (
+        <View style={Styles.containerModal}>
+            <Modal
+                transparent={transparent}
+                animationType={animationType}
+                visible={visible}
+                onRequestClose={() => {
+                    ShowModalFunction(!ModalVisibleStatus, '');
+                }}>
+                    <View style={Styles.modelStyle}>
+                        <Image
+                            style={Styles.fullImageStyle}
+                            source={{ uri: imageuri }}
+                        />
+                        <TouchableOpacity
+                            activeOpacity={0.5}
+                            style={Styles.closeButtonStyle}
+                            onPress={() => {
+                                ShowModalFunction(!ModalVisibleStatus, '');
+                            }}>
+                            <Image
+                                source={{
+                                    uri:
+                                        'http://www.scalsys.com/1pngs/close-png/close_png_288728.png',
+                                }}
+                                style={{ width: 30, height: 30, marginTop: 20 }}
+                            />
+                        </TouchableOpacity>
+                    </View>
+            </Modal>
+
+        </View>
+
+
+    );
+
+export default ModalComponent;
