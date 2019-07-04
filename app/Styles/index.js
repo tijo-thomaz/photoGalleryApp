ebdee9d6b1869ceca5433ec3f81bd2fa404de1ac
Tijo <tijo@QB-TP3-1605.local>
2019-07-04 13:02:17 +0530

    addedStyling

---
+++ app/Styles/index.js
@@ -0,0 +1,17 @@
+
+import {
+    StyleSheet,
+} from 'react-native';
+
+export default Styles = StyleSheet.create({
+    containerStyle: {
+        justifyContent: 'center',
+        flex: 1,
+        marginTop: 20,
+    },
+    row: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+    },
+});
